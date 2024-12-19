c62e27c7a13f7a6638dcddfaf7453b427133396e
Andrei Yankavets <andreyyankovez@gmail.com>
2024-12-19 18:52:35 +0300

    [FEATURE] Add validation in CreateUserDto and sign_up page

--- src/main/java/by/yankavets/dto/CreateUserDto.java
+++ src/main/java/by/yankavets/dto/CreateUserDto.java
@@ -8,7 +8,7 @@ import lombok.*;
 public class CreateUserDto {
 
     @NotBlank(message = "Name should not be empty!")
-    @Pattern(regexp = "^[a-zA-Z0-9._-]{2,32}$\n", message = "Name should be between 2 and 32 characters")
+    @Pattern(regexp = "^[a-zA-Z0-9._-]{2,32}$", message = "Name should be between 2 and 32 characters")
     private String name;
 
     @Pattern(regexp = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$", message = "Email is not valid")
