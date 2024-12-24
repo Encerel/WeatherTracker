9e59a5c345a772a38edc95ceb20021613011867a
Andrei Yankavets <andreyyankovez@gmail.com>
2024-12-24 17:51:07 +0300

    [REFACTOR] Fix regex and message

--- src/main/java/by/yankavets/dto/UserCreateDto.java
+++ src/main/java/by/yankavets/dto/UserCreateDto.java
@@ -5,20 +5,20 @@ import lombok.*;
 
 @Data
 @RequiredArgsConstructor
-public class CreateUserDto {
+public class UserCreateDto {
 
     @NotBlank(message = "Name should not be empty!")
-    @Pattern(regexp = "^[a-zA-Z0-9._-]{2,32}$", message = "Name should be between 2 and 32 characters")
+    @Pattern(regexp = "^[a-zA-Zа-яА-Я0-9._-]{2,32}$", message = "Name should be between 2 and 32 characters")
     private String name;
 
     @Pattern(regexp = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$", message = "Email is not valid")
     private String email;
 
-    @Size(min = 8, max = 64, message = "Password should be between 8 and 30 characters")
+    @Size(min = 8, max = 64, message = "Password should be between 8 and 64 characters")
     @NotBlank(message = "Password should not be empty!")
     private String password;
 
-    @Size(min = 8, max = 64, message = "Name should be between 8 and 30 characters")
+    @Size(min = 8, max = 64, message = "Password should be between 8 and 64 characters")
     @NotBlank(message = "Confirmed password should not be empty!")
     private String confirmedPassword;
 
