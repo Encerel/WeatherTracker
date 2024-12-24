da4379c81692439783366398ca65c9d00f3a6b7f
Andrei Yankavets <andreyyankovez@gmail.com>
2024-12-24 17:56:03 +0300

    [FEATURE] Create constants classes ColumnName and ParametersAndAttribute

---
+++ src/main/java/by/yankavets/constant/ColumnName.java
@@ -0,0 +1,10 @@
+package by.yankavets.constant;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class ColumnName {
+
+    public static final String EMAIL = "email";
+    public static final String PASSWORD = "password";
+}
---
+++ src/main/java/by/yankavets/constant/ParametersAndAttribute.java
@@ -0,0 +1,12 @@
+package by.yankavets.constant;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class ParametersAndAttribute {
+
+    public static final String USER = "user";
+    public static final String SUCCESS = "success";
+    public static final String ERROR = "error";
+    public static final String SESSION_UUID = "message";
+}
