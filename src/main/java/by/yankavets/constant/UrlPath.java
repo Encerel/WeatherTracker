c579d153baea28365aba8cddcd795564148f8fc9
Andrei Yankavets <andreyyankovez@gmail.com>
2024-12-26 18:13:05 +0300

    [FEATURE] Create util class UrlPath

---
+++ src/main/java/by/yankavets/constant/UrlPath.java
@@ -0,0 +1,14 @@
+package by.yankavets.constant;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class UrlPath {
+
+    public static final String SIGN_IN_URL = "/sign_in";
+    public static final String SIGN_IN_PAGE = "sign_in";
+    public static final String SIGN_UP_URL = "/sign_up";
+    public static final String SIGN_UP_PAGE = "sign_up";
+    public static final String INDEX_URL = "/";
+    public static final String INDEX_PAGE = "index";
+}
