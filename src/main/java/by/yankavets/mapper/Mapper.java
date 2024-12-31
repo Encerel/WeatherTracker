990576084c17181666e73dcade302ceaa1f4ee0f
Andrei Yankavets <andreyyankovez@gmail.com>
2024-12-31 17:36:36 +0300

    [ADD] Add unsupported method handler

--- src/main/java/by/yankavets/mapper/Mapper.java
+++ src/main/java/by/yankavets/mapper/Mapper.java
@@ -1,10 +1,23 @@
 package by.yankavets.mapper;
 
+import by.yankavets.exception.UnsupportedMapMethodException;
+
+import java.lang.reflect.Method;
+import java.util.List;
+
 public interface Mapper<F, T> {
 
     T mapToDto(F entity);
 
-    default F mapToEntity(T dto) {
-        return null;
+    default F mapToEntity(T dto) throws NoSuchMethodException {
+        Class<?> clazz = Mapper.class;
+        Method method = clazz.getMethod("mapToEntity", Object.class);
+        throw new UnsupportedMapMethodException(method.getName());
+    }
+
+    default List<T> mapToListDto(List<F> entitiesList) throws NoSuchMethodException {
+        Class<?> clazz = Mapper.class;
+        Method method = clazz.getMethod("mapToListDto", List.class);
+        throw new UnsupportedMapMethodException(method.getName());
     }
 }
