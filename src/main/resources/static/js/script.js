e7360d9185070383036f0404be5d67b86fcde9f3
Andrei Yankavets <andreyyankovez@gmail.com>
2024-12-31 17:22:20 +0300

    [ADD] Add function, which clear error param from url

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -5,3 +5,11 @@ document.addEventListener("DOMContentLoaded", function() {
         window.history.replaceState({}, document.title, url.toString());
     }
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+    const url = new URL(window.location.href);
+    if (url.searchParams.has('error')) {
+        url.searchParams.delete('error');
+        window.history.replaceState({}, document.title, url.toString());
+    }
+});
