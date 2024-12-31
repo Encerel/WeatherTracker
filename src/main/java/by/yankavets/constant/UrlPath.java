9f4468375a9f4c6c08f7f13b4caa45f3cca3cb6d
Andrei Yankavets <andreyyankovez@gmail.com>
2024-12-31 17:21:23 +0300

    [ADD] Add new path url in class

--- src/main/java/by/yankavets/constant/UrlPath.java
+++ src/main/java/by/yankavets/constant/UrlPath.java
@@ -6,9 +6,18 @@ import lombok.experimental.UtilityClass;
 public class UrlPath {
 
     public static final String SIGN_IN_URL = "/sign_in";
-    public static final String SIGN_IN_PAGE = "sign_in";
     public static final String SIGN_UP_URL = "/sign_up";
-    public static final String SIGN_UP_PAGE = "sign_up";
     public static final String INDEX_URL = "/";
+    public static final String LOGOUT_URL = "/logout";
+    public static final String CSS_URL = "/css/**";
+    public static final String JS_URL = "/js/**";
+    public static final String IMAGE_URL = "/image/**";
+    public static final String ALL_PAGES_URL_PATTERN = "/**";
+
+
+
+    public static final String SIGN_IN_PAGE = "sign_in";
+    public static final String SIGN_UP_PAGE = "sign_up";
     public static final String INDEX_PAGE = "index";
+
 }
