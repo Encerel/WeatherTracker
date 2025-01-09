9b58e1053800ca3c0f155c4b8f0fe0af36105e0d
Andrei Yankavets <andreyyankovez@gmail.com>
2025-01-09 14:36:34 +0300

    [ADD] Add new urls

--- src/main/java/by/yankavets/constant/UrlPath.java
+++ src/main/java/by/yankavets/constant/UrlPath.java
@@ -11,13 +11,20 @@ public class UrlPath {
     public static final String LOGOUT_URL = "/logout";
     public static final String CSS_URL = "/css/**";
     public static final String JS_URL = "/js/**";
-    public static final String IMAGE_URL = "/image/**";
+    public static final String IMAGE_URL = "/images/**";
     public static final String ALL_PAGES_URL_PATTERN = "/**";
+    public static final String SEARCH_URL = "/search";
+    public static final String DELETE_URL = "/delete";
+    public static final String LOCATIONS_URL = "/locations";
+
+
 
 
 
     public static final String SIGN_IN_PAGE = "sign_in";
     public static final String SIGN_UP_PAGE = "sign_up";
     public static final String INDEX_PAGE = "index";
-
+    public static final String SEARCH_PAGE = "search";
+    public static final String ERROR_PAGE = "error";
+    public static final String REDIRECT = "redirect:";
 }
