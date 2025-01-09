f7a1c015a64c48a6fcbeb5caa62e65dac6f9bc0e
Andrei Yankavets <andreyyankovez@gmail.com>
2025-01-09 13:38:32 +0300

    [ADD] Add new parameters

--- src/main/java/by/yankavets/constant/ParametersAndAttribute.java
+++ src/main/java/by/yankavets/constant/ParametersAndAttribute.java
@@ -8,5 +8,22 @@ public class ParametersAndAttribute {
     public static final String USER = "user";
     public static final String SUCCESS = "success";
     public static final String ERROR = "error";
-    public static final String SESSION_UUID = "message";
+    public static final String SESSION_UUID = "session_uuid";
+    public static final String USER_NAME = "user_name";
+    public static final String LOCATION_NAME = "location_name";
+    public static final String ID = "id";
+    public static final String QUERY = "q";
+    public static final String UNITS = "units";
+    public static final String LANGUAGE = "lang";
+    public static final String APP_ID = "appid";
+    public static final String LOCATION_LIST = "location_list";
+    public static final String LOCATION = "location";
+    public static final String WEATHER_BY_LOCATION_LIST = "weather_by_location_list";
+    public static final String LIMIT = "limit";
+    public static final String LATITUDE = "latitude";
+    public static final String LONGITUDE = "longitude";
+    public static final String LAT = "lat";
+    public static final String LON = "lon";
+
+
 }
