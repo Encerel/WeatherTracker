ad1014be4c29ded4602fc1c79177ccaa1784aed0
Andrei Yankavets <andreyyankovez@gmail.com>
2025-01-09 13:37:31 +0300

    [FIX] Fix login interceptor

--- src/main/java/by/yankavets/interceptor/LoginInterceptor.java
+++ src/main/java/by/yankavets/interceptor/LoginInterceptor.java
@@ -1,13 +1,12 @@
 package by.yankavets.interceptor;
 
-import by.yankavets.dto.UserReadDto;
-import by.yankavets.entity.SessionEntity;
-import by.yankavets.exception.InvalidSessionException;
+import by.yankavets.exception.session.InvalidSessionException;
+import by.yankavets.exception.session.SessionExpiredException;
+import by.yankavets.model.entity.SessionEntity;
 import by.yankavets.service.SessionService;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
@@ -16,17 +15,20 @@ import org.springframework.web.servlet.HandlerInterceptor;
 import java.io.IOException;
 import java.time.LocalDateTime;
 import java.util.Optional;
+import java.util.Set;
 import java.util.UUID;
 
-import static by.yankavets.constant.ParametersAndAttribute.*;
+import static by.yankavets.constant.ParametersAndAttribute.ERROR;
+import static by.yankavets.constant.ParametersAndAttribute.SESSION_UUID;
 import static by.yankavets.constant.UrlPath.SIGN_IN_URL;
+import static by.yankavets.constant.UrlPath.SIGN_UP_URL;
 
 
 @Component
 public class LoginInterceptor implements HandlerInterceptor {
 
     private final SessionService sessionService;
-
+    private static final Set<String> PUBLIC_PATH = Set.of(SIGN_IN_URL, SIGN_UP_URL);
     @Autowired
     public LoginInterceptor(@Lazy SessionService sessionService) {
         this.sessionService = sessionService;
@@ -36,34 +38,32 @@ public class LoginInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
 
-        HttpSession session = request.getSession(false);
         try {
-            if (session != null && session.getAttribute(USER) != null) {
-                UserReadDto userReadDto = (UserReadDto) session.getAttribute(USER);
-                Optional<UUID> sessionId = findSessionId(request.getCookies());
-                if (sessionId.isPresent()) {
-                    Optional<SessionEntity> foundSession = sessionService.findByUserAndSessionId(
-                            userReadDto,
-                            sessionId.get()
-                    );
-                    if (foundSession.isPresent()) {
-                        SessionEntity sessionEntity = foundSession.get();
-                        if (sessionEntity.getExpireAt().isBefore(LocalDateTime.now())) {
-                            session.invalidate();
-                            throw new InvalidSessionException();
-                        }
-                        return true;
-                    }
-                }
+            Optional<UUID> sessionId = findSessionId(request.getCookies());
+
+            if (sessionId.isEmpty()) {
+                reject(request, response);
+                return false;
             }
+
+            UUID uuid = sessionId.get();
+            Optional<SessionEntity> foundSession = sessionService.findById(uuid);
+
+            if (foundSession.isEmpty()) {
+                throw new InvalidSessionException();
+            }
+
+            SessionEntity sessionEntity = foundSession.get();
+
+            if (sessionEntity.getExpireAt().isBefore(LocalDateTime.now())) {
+                throw new SessionExpiredException();
+            }
+            return true;
         } catch (RuntimeException e) {
             request.setAttribute(ERROR, e.getMessage());
             request.getRequestDispatcher(SIGN_IN_URL).forward(request, response);
             return false;
         }
-
-        reject(request, response);
-        return false;
     }
 
     private Optional<UUID> findSessionId(Cookie[] cookies) {
@@ -77,7 +77,11 @@ public class LoginInterceptor implements HandlerInterceptor {
 
     private void reject(HttpServletRequest request, HttpServletResponse response) throws IOException {
         String prevPage = request.getHeader("referer");
-        response.sendRedirect(prevPage != null ? prevPage : SIGN_IN_URL);
+        response.sendRedirect(prevPage != null && isPublicPath(prevPage) ? prevPage : SIGN_IN_URL);
+    }
+
+    private boolean isPublicPath(String path) {
+        return PUBLIC_PATH.stream().anyMatch(path::startsWith);
     }
 
 
