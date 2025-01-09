146f31f300248526599f8b513de5ee4ab8c2c547
Andrei Yankavets <andreyyankovez@gmail.com>
2025-01-09 14:43:03 +0300

    [ADD] Add new lombok annotations

--- src/main/java/by/yankavets/dto/user/UserLoginDto.java
+++ src/main/java/by/yankavets/dto/user/UserLoginDto.java
@@ -1,15 +1,18 @@
-package by.yankavets.dto;
+package by.yankavets.dto.user;
 
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
-import lombok.RequiredArgsConstructor;
-import lombok.Value;
+import lombok.NoArgsConstructor;
 
 @Data
-@RequiredArgsConstructor
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class UserLoginDto {
 
     @Pattern(regexp = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$", message = "Email is not valid")
