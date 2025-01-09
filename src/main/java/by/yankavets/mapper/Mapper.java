9e7f62330b59a05a0c06da832cba060ae8c6d458
Andrei Yankavets <andreyyankovez@gmail.com>
2025-01-09 13:38:14 +0300

    [REFACTOR] Make all methods in mapper default

--- src/main/java/by/yankavets/mapper/Mapper.java
+++ src/main/java/by/yankavets/mapper/Mapper.java
@@ -1,13 +1,17 @@
 package by.yankavets.mapper;
 
-import by.yankavets.exception.UnsupportedMapMethodException;
+import by.yankavets.exception.mapper.UnsupportedMapMethodException;
 
 import java.lang.reflect.Method;
 import java.util.List;
 
 public interface Mapper<F, T> {
 
-    T mapToDto(F entity);
+    default T mapToDto(F entity) throws NoSuchMethodException {
+        Class<?> clazz = Mapper.class;
+        Method method = clazz.getMethod("mapToDto", Object.class);
+        throw new UnsupportedMapMethodException(method.getName());
+    }
 
     default F mapToEntity(T dto) throws NoSuchMethodException {
         Class<?> clazz = Mapper.class;
